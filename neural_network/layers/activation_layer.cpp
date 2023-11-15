7a079241c47d7aeecef0cb2ca8fad7e0fb5d2555
Anish-M <anish.mankal@gmail.com>
2023-11-15 01:31:32 -0600

    fc_layer

--- neural_network/layers/activation_layer.cpp
+++ neural_network/layers/activation_layer.cpp
@@ -2,11 +2,12 @@
 #include <vector>
 
 using namespace std;
+using std::vector;
 
 class ActivationLayer : public Layer {
 private:
-    std::vector<double> input;
-    std::vector<double> output;
+    vector<double> input;
+    vector<double> output;
     std::function<double(double)> activation;
     std::function<double(double)> activation_prime;
 
@@ -18,7 +19,7 @@ public:
         : activation(activation_function), activation_prime(activation_prime_function) {}
 
     // Forward propagation
-    std::vector<double> forward_propagation(const std::vector<double>& input_data) {
+    vector<double> forward_propagation(const vector<double>& input_data) {
         this->input = input_data;
         this->output.clear();
         for (double i : input) {
@@ -28,8 +29,8 @@ public:
     }
 
     // Backward propagation
-    std::vector<double> backward_propagation(const std::vector<double>& output_error) {
-        std::vector<double> input_error;
+    vector<double> backward_propagation(const vector<double>& output_error) {
+        vector<double> input_error;
         for (size_t i = 0; i < this->input.size(); ++i) {
             input_error.push_back(this->activation_prime(this->input[i]) * output_error[i]);
         }
