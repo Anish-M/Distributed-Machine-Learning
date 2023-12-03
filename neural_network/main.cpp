83429a180a3b59ef89a8befe2d87773be71d2f7c
Pranav Tiwari <pranavt@cs.utexas.edu>
2023-12-02 19:56:40 -0600

    message info

--- neural_network/main.cpp
+++ neural_network/main.cpp
@@ -8,6 +8,7 @@
 #include "FCLayer.hpp"
 #include "ActivationLayer.hpp"
 #include "Activations.hpp"
+#include <pthread.h>
 
 using namespace std;
 
@@ -124,8 +125,115 @@ int runExample() {
     return 0;
 }
 
+int dataParallel() {
+    
+
+}
+
 int main() {
     runGenerated();
 
     return 0;
 }
+
+
+int splitDataMaster(int numWorkers) {
+    
+    // Read x_train and y_train from file
+    vector<vector<double>> x_train;
+    vector<vector<double>> y_train;
+
+    // Read x_train and y_train from file 
+    int n_samples, n_features, n_classes;
+    n_samples = 10;
+    n_features = 5;
+    n_classes = 1;
+    string s;
+
+    // randomly shuffle the indices, but seed it so that it is the same every time
+    srand(0);
+    vector<int> indices(n_samples);
+    iota(indices.begin(), indices.end(), 0);
+    random_shuffle(indices.begin(), indices.end());
+
+    // split the indices into numWorkers chunks
+    vector<vector<int>> chunks(numWorkers);
+    int chunkSize = n_samples / numWorkers;
+    for (int i = 0; i < numWorkers; i++) {
+        for (int j = i * chunkSize; j < (i + 1) * chunkSize; j++) {
+            chunks[i].push_back(indices[j]);
+        }
+    }
+
+    // print out the chunks per worker
+    for (int i = 0; i < numWorkers; i++) {
+        cout << "Worker " << i << ": ";
+        for (int j = 0; j < chunks[i].size(); j++) {
+            cout << chunks[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+
+int readInDataWorker(int numWorkers) {
+    
+    // Read x_train and y_train from file
+    vector<vector<double>> x_train;
+    vector<vector<double>> y_train;
+
+    // Read x_train and y_train from file 
+    int n_samples, n_features, n_classes;
+    n_samples = 10;
+    n_features = 5;
+    n_classes = 1;
+    string s;
+    
+    // the first line is 'X'
+    freopen("../data/generated_10_2_5.txt", "r", stdin);
+
+    // read in X train
+    cin >> s;
+    cout << s << endl;
+    for (int i = 0; i < n_samples; i++) {
+        vector<double> x;
+        for (int j = 0; j < n_features; j++) {
+            double temp;
+            cin >> temp;
+            x.push_back(temp);
+        }
+        x_train.push_back(x);
+    }
+
+    // read in Y train
+    cin >> s;
+    cout << s << endl;
+    for (int i = 0; i < n_samples; i++) {
+        vector<double> y;
+        for (int j = 0; j < n_classes; j++) {
+            double temp;
+            cin >> temp;
+            y.push_back(temp);
+        }
+        y_train.push_back(y);
+    }
+
+    string indices_to_read;
+
+    // make an my_x_train and my_y_train including each index in the indices_to_read
+    vector<vector<double>> my_x_train;
+    vector<vector<double>> my_y_train;
+
+    // loop through the indices_to_read and add the corresponding x_train and y_train to my_x_train and my_y_train
+    for (int i = 0; i < indices_to_read.size(); i++) {
+        int index = stoi(indices_to_read[i]);
+        my_x_train.push_back(x_train[index]);
+        my_y_train.push_back(y_train[index]);
+    }
+
+    // print out the my_x_train and my_y_train's dimensions
+    cout << "my_x_train size: " << my_x_train.size() << endl;
+    cout << "my_y_train size: " << my_y_train.size() << endl;
+}
+
+
